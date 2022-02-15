e9521d95ccecd271b9ab0fa460cc7270fb96351f
Krzysztof Markiewicz <krismarkiew@gmail.com>
2022-02-15 21:00:41 +0100

    change content in index.html, change on.off nav-mobile

--- js/nav-mobile.js
+++ js/nav-mobile.js
@@ -1,9 +1,22 @@
 let nav = document.getElementById('main-nav')
 
+
+//on/off mobile navigation
+document.addEventListener('click', function (event) {
+	if (event.target.classList.contains('fas')) {
+		nav.classList.toggle('is-open')
+	}else{nav.classList.remove('is-open')}
+})
+
+
+// old version on/off menu mobile
+/*
 burger.addEventListener('click', function () {
 	nav.classList.toggle('is-open');
 });
 
 nav.addEventListener('click', function () {
 	nav.classList.remove('is-open')
+
 })
+*/
